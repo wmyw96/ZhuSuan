3aac7962fd51d940eff537bcb3cb9a1de1ad72d6
Jiaxin <ishijiaxin@126.com>
2016-10-04 16:24:05 +0800

    2-layer stochastic

--- examples/vae_colored.py
+++ examples/vae_colored.py
@@ -37,14 +37,23 @@ class M1:
     :param n_z: Int. The dimension of latent variables (z).
     :param n_x: Int. The dimension of observed variables (x).
     """
-    def __init__(self, n_z, n_x):
-        self.n_z = n_z
+    def __init__(self, n_x):
         self.n_x = n_x
         with pt.defaults_scope(activation_fn=tf.nn.relu):
-            self.l_x_z = (pt.template('z').
-                          reshape([-1, 16, 16, 32]).
-                          deconv2d(3, 64).
-                          deconv2d(5, 3, stride=2, activation_fn=None))
+            self.l_z1_z2 = (pt.template('z2').
+                            reshape([-1, 16, 16, 64]).
+                            deconv2d(3, 64))
+
+            self.l_z1_mean = (pt.template('z1_hid').
+                              deconv2d(3, 64, activation_fn=None))
+
+            self.l_z1_logvar = (pt.template('z1_hid').
+                                deconv2d(3, 64, activation_fn=None))
+
+            self.l_x_z1 = (pt.template('z1').
+                           reshape([-1, 16, 16, 64]).
+                           deconv2d(5, 64, stride=2).
+                           deconv2d(3, 3, activation_fn=None))
 
             self.x_logsd = tf.get_variable("x_logsd",  (),
                                            initializer=tf.constant_initializer(
@@ -62,21 +71,31 @@ class M1:
         :return: A Tensor of shape (batch_size, n_samples). The joint log
             likelihoods.
         """
-        z = latent['z']
+        z2 = latent['z2']
+        z1 = latent['z1']
         x = observed['x']
 
-        n_samples = tf.shape(z)[1]
-        x_mean = self.l_x_z.construct(
-            z=tf.reshape(z, (-1, self.n_z))).reshape(
-            (-1, n_samples, self.n_x)).tensor
+        log_pz2 = tf.reduce_sum(norm.logpdf(z2), [2, 3, 4])
+
+        n_samples = tf.shape(z1)[1]
+        z1_hid = self.l_z1_z2.construct(z2=z2).tensor
+        z1_mean = self.l_z1_mean.construct(z1_hid=z1_hid).reshape(
+            [-1, n_samples, 16, 16, 64]).tensor
+        z1_logvar = self.l_z1_logvar.construct(z1_hid=z1_hid).reshape(
+            [-1, n_samples, 16, 16, 64]).tensor
+        log_pz1_z2 = norm.logpdf(z1, z1_mean, tf.exp(0.5 * z1_logvar))
+        log_pz1_z2 = tf.reduce_sum(log_pz1_z2, [2, 3, 4])
+
+        x_mean = self.l_x_z1.construct(z1=z1).reshape(
+            [-1, n_samples, self.n_x]).tensor
         x_scale = tf.exp(self.x_logsd)
         x = tf.expand_dims(x, 1)
         x = tf.clip_by_value(x, 0, 1)
-        log_px_z = tf.log(logistic.cdf(x + 1. / 256, x_mean, x_scale) -
-                          logistic.cdf(x, x_mean, x_scale) + 1e-8)
-        log_px_z = tf.reduce_sum(log_px_z, 2)
-        log_pz = tf.reduce_sum(norm.logpdf(z), 2)
-        return log_px_z + log_pz
+        log_px_z1 = tf.log(logistic.cdf(x + 1. / 256, x_mean, x_scale) -
+                           logistic.cdf(x, x_mean, x_scale) + 1e-8)
+        log_px_z1 = tf.reduce_sum(log_px_z1, 2)
+
+        return log_px_z1 + log_pz1_z2 + log_pz2
 
 
 def q_net(n_x, n_xl, n_z, n_samples):
@@ -92,18 +111,33 @@ def q_net(n_x, n_xl, n_z, n_samples):
     """
     with pt.defaults_scope(activation_fn=tf.nn.relu):
         lx = InputLayer((None, n_x))
-        lz_x = PrettyTensor({'x': lx},
-                            pt.template('x').
-                            reshape([-1, n_xl, n_xl, 3]).
-                            conv2d(5, 64, stride=2))
-        lz_mean = PrettyTensor({'z': lz_x}, pt.template('z').
-                               conv2d(3, 32, activation_fn=None).
-                               reshape((-1, 1, n_z)))
-        lz_logvar = PrettyTensor({'z': lz_x}, pt.template('z').
-                                 conv2d(3, 32, activation_fn=None).
-                                 reshape((-1, 1, n_z)))
-        lz = Normal([lz_mean, lz_logvar], n_samples)
-    return lx, lz
+        lz1_x = PrettyTensor({'x': lx},
+                             pt.template('x').
+                             reshape([-1, n_xl, n_xl, 3]).
+                             conv2d(3, 64))
+        lz1_mean = PrettyTensor({'z1_hid': lz1_x},
+                                pt.template('z1_hid').
+                                conv2d(5, 64, stride=2, activation_fn=None).
+                                reshape([-1, 1, 16, 16, 64]))
+        lz1_logvar = PrettyTensor({'z1_hid': lz1_x},
+                                  pt.template('z1_hid').
+                                  conv2d(5, 64, stride=2, activation_fn=None).
+                                  reshape([-1, 1, 16, 16, 64]))
+        lz1 = Normal([lz1_mean, lz1_logvar], n_samples)
+        lz2_z1 = PrettyTensor({'z1': lz1},
+                              pt.template('z1').
+                              reshape([-1, 16, 16, 64]).
+                              conv2d(3, 64))
+        lz2_mean = PrettyTensor({'z2_hid': lz2_z1},
+                                pt.template('z2_hid').
+                                conv2d(3, 64, activation_fn=None).
+                                reshape([-1, n_samples, 16, 16, 64]))
+        lz2_logvar = PrettyTensor({'z2_hid': lz2_z1},
+                                  pt.template('z2_hid').
+                                  conv2d(3, 64, activation_fn=None).
+                                  reshape([-1, n_samples, 16, 16, 64]))
+        lz2 = Normal([lz2_mean, lz2_logvar])
+    return lx, lz1, lz2
 
 
 if __name__ == "__main__":
@@ -123,10 +157,8 @@ if __name__ == "__main__":
     x_test = x_test.reshape((-1, n_x))
     n_y = t_train.shape[1]
 
-    # Define model parameters
-    n_z = 16 * 16 * 32
     # Define training/evaluation parameters
-    lb_samples = 1
+    lb_samples = 10
     ll_samples = 100
     epoches = 3000
     batch_size = 16
@@ -141,35 +173,41 @@ if __name__ == "__main__":
     def build_model(phase, reuse=False):
         with pt.defaults_scope(phase=phase):
             with tf.variable_scope("model", reuse=reuse) as scope:
-                model = M1(n_z, n_x)
+                model = M1(n_x)
             with tf.variable_scope("variational", reuse=reuse) as scope:
-                lx, lz = q_net(n_x, n_xl, n_z, n_samples)
-        return model, lx, lz
+                lx, lz1, lz2 = q_net(n_x, n_xl, n_z, n_samples)
+        return model, lx, lz1, lz2
 
     # Build the training computation graph
     learning_rate_ph = tf.placeholder(tf.float32, shape=[])
     x = tf.placeholder(tf.float32, shape=(None, n_x))
     n_samples = tf.placeholder(tf.int32, shape=())
     optimizer = tf.train.AdamOptimizer(learning_rate_ph, epsilon=1e-4)
-    model, lx, lz = build_model(pt.Phase.train)
-    z_outputs = get_output(lz, x)
+    model, lx, lz1, lz2 = build_model(pt.Phase.train)
+    z1_outputs, z2_outputs = get_output([lz1, lz2], x)
+    latent = {'z1': z1_outputs, 'z2': z2_outputs}
     lower_bound = tf.reduce_mean(advi(
-        model, {'x': x}, {'z': z_outputs}, reduction_indices=1))
+        model, {'x': x}, latent, reduction_indices=1))
     bits_per_dim = -lower_bound / n_x * 1. / np.log(2.)
     grads = optimizer.compute_gradients(bits_per_dim)
-    grad_range = reduce(tf.maximum, list(
-        tf.reduce_max(tf.abs(k)) for k, v in grads if k is not None))
+    def l2_norm(x):
+        return tf.sqrt(tf.reduce_sum(tf.square(x)))
+    update_ratio = learning_rate_ph * tf.reduce_mean(tf.pack(list(
+        (l2_norm(k) / (l2_norm(v) + 1e-8)) for k, v in grads
+        if k is not None)))
     infer = optimizer.apply_gradients(grads)
 
     # Build the evaluation computation graph
-    eval_model, eval_lx, eval_lz = build_model(pt.Phase.test, reuse=True)
-    z_outputs = get_output(eval_lz, x)
+    eval_model, eval_lx, eval_lz1, eval_lz2 = build_model(
+        pt.Phase.test, reuse=True)
+    z1_outputs, z2_outputs = get_output([eval_lz1, eval_lz2], x)
+    eval_latent = {'z1': z1_outputs, 'z2': z2_outputs}
     eval_lower_bound = tf.reduce_mean(advi(
-        eval_model, {'x': x}, {'z': z_outputs}, reduction_indices=1))
+        eval_model, {'x': x}, latent, reduction_indices=1))
     eval_bits_per_dim = -eval_lower_bound / n_x * 1. / np.log(2.)
     eval_log_likelihood = tf.reduce_mean(is_loglikelihood(
-        eval_model, {'x': x}, {'z': z_outputs}, reduction_indices=1))
-    eval_bits_per_dim_ll = eval_log_likelihood / n_x * 1. / np.log(2.)
+        eval_model, {'x': x}, latent, reduction_indices=1))
+    eval_bits_per_dim_ll = -eval_log_likelihood / n_x * 1. / np.log(2.)
 
     params = tf.trainable_variables()
     for i in params:
@@ -187,22 +225,22 @@ if __name__ == "__main__":
             np.random.shuffle(x_train)
             lbs = []
             bitss = []
-            grad_ranges = []
+            update_ratios = []
             for t in range(iters):
                 x_batch = x_train[t * batch_size:(t + 1) * batch_size]
 
-                _, lb, bits, grad_range_ = sess.run([infer, lower_bound,
-                                               bits_per_dim, grad_range],
+                _, lb, bits, update_ratio_ = sess.run([infer, lower_bound,
+                                               bits_per_dim, update_ratio],
                                  feed_dict={x: x_batch,
                                             learning_rate_ph: learning_rate,
                                             n_samples: lb_samples})
-                grad_ranges.append(grad_range_)
+                update_ratios.append(update_ratio_)
                 lbs.append(lb)
                 bitss.append(bits)
             time_epoch += time.time()
             print('Epoch {} ({:.1f}s): Lower bound = {} bits = {}'.format(
                 epoch, time_epoch, np.mean(lbs), np.mean(bitss)))
-            print('updates={}'.format(learning_rate * np.max(grad_ranges)))
+            print('update ratio = {}'.format(np.mean(update_ratios)))
             if epoch % test_freq == 0:
                 time_test = -time.time()
                 test_lbs = []
