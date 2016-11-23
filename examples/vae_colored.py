f969e57ded46e49f774b0770f2c93401e8a6e743
xinmei9322 <luoyucen9322@qq.com>
2016-11-23 17:00:32 +0800

    vae_colored minor

--- examples/vae_colored.py
+++ examples/vae_colored.py
@@ -12,13 +12,13 @@ import tensorflow as tf
 from tensorflow.contrib import layers
 from six.moves import range
 import numpy as np
-
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 try:
     from zhusuan.model import *
     from zhusuan.variational import advi
     from zhusuan.evaluation import is_loglikelihood
     from zhusuan.distributions import logistic
+    from zhusuan.optimization.adamax import AdamaxOptimizer
 except:
     raise ImportError()
 
@@ -42,12 +42,27 @@ class M1:
         normalizer_params = {'is_training': is_training,
                              'updates_collections': None}
         with StochasticGraph() as model:
-            z1_mean = tf.zeros([n_particles*n, 16, 16, 64])
-            z1_logstd = tf.zeros([n_particles*n, 16, 16, 64])
-            z1 = Normal(z1_mean, z1_logstd)
-            l_z0_z1 = layers.conv2d_transpose(tf.reshape(z1.value, [-1, 16, 16, 64]), 64, 3)
-            z0_mean = layers.conv2d_transpose(l_z0_z1, 64, 3, activation_fn=None)
+            z3_mean = tf.zeros([n_particles*n, 4, 4, 256])
+            z3_logstd = tf.zeros([n_particles*n, 4, 4, 256])
+            z3 = Normal(z3_mean, z3_logstd)
+            l_z2_z3 = layers.conv2d_transpose(z3.value, 256, 3)
+            l_z2_z3 = layers.conv2d_transpose(l_z2_z3, 256, 3)
+            l_z2_z3 = layers.conv2d_transpose(l_z2_z3, 128, 5, stride=2)
 
+            z2_mean = layers.conv2d_transpose(l_z2_z3, 128, 3, activation_fn=None)
+            z2_logstd = layers.conv2d_transpose(l_z2_z3, 128, 3, activation_fn=None)
+            z2 = Normal(z2_mean, z2_logstd)
+            l_z1_z2 = layers.conv2d_transpose(z2.value, 128, 3)
+            l_z1_z2 = layers.conv2d_transpose(l_z1_z2, 128, 3)
+            l_z1_z2 = layers.conv2d_transpose(l_z1_z2, 64, 5, stride=2)
+
+            z1_mean = layers.conv2d_transpose(l_z1_z2, 64, 3, activation_fn=None)
+            z1_logstd = layers.conv2d_transpose(l_z1_z2, 64, 3, activation_fn=None)
+            z1 = Normal(z1_mean, z1_logstd)
+
+            l_z0_z1 = layers.conv2d_transpose(z1.value, 64, 3)
+            l_z0_z1 = layers.conv2d_transpose(l_z0_z1, 64, 3)
+            z0_mean = layers.conv2d_transpose(l_z0_z1, 64, 3, activation_fn=None)
             z0_logstd = layers.conv2d_transpose(l_z0_z1, 64, 3, activation_fn=None)
 
             z0 = Normal(z0_mean, z0_logstd)
@@ -57,7 +72,7 @@ class M1:
             x = tf.reshape(x, [n_particles, n, n_x])
         self.model = model
         self.x = x
-        self.zs = [z0, z1]
+        self.zs = [z0, z1, z2, z3]
         self.n_particles = n_particles
         self.n_x = n_x
         self.x_logsd = tf.get_variable("x_logsd", (),
@@ -74,19 +89,30 @@ class M1:
 
         :return: A Tensor. The joint log likelihoods.
         """
-        z0, z1 = latent['z0'], latent['z1']
+        z0, z1, z2, z3 = latent['z0'], latent['z1'], latent['z2'], latent['z3']
         x = observed['x']
         x = tf.tile(tf.expand_dims(x, 0), [n_particles, 1, 1])
-        z0_out, z1_out, x_out = self.model.get_output(self.zs + [self.x],
-                                                      inputs={self.zs[0]: z0,
-                                                              self.zs[1]: z1})
+        z0_out, z1_out, z2_out, z3_out, x_out = self.model.get_output(
+            self.zs + [self.x],
+            inputs={self.zs[0]: z0,
+                    self.zs[1]: z1,
+                    self.zs[2]: z2,
+                    self.zs[3]: z3
+                    })
         x_mean = x_out[0]
         log_px_z0 = tf.log(logistic.cdf(x + 1. / 256, x_mean, self.x_logsd) -
                            logistic.cdf(x, x_mean, self.x_logsd) + 1e-8)
         log_px_z0 = tf.reduce_sum(log_px_z0, -1)
-        log_pz0_z1 = tf.reduce_sum(tf.reshape(z0_out[1], [n_particles, -1, 16, 16, 64]), [2, 3, 4])
-        log_pz1 = tf.reduce_sum(tf.reshape(z1_out[1], [n_particles, -1, 16, 16, 64]), [2, 3, 4])
-        return log_px_z0 + log_pz1 + log_pz0_z1
+        log_pz0_z1 = tf.reduce_sum(
+            tf.reshape(z0_out[1], [n_particles, -1, 16, 16, 64]), [2, 3, 4])
+        log_pz1_z2 = tf.reduce_sum(
+            tf.reshape(z1_out[1], [n_particles, -1, 16, 16, 64]), [2, 3, 4])
+        log_pz2_z3 = tf.reduce_sum(
+            tf.reshape(z2_out[1], [n_particles, -1, 8, 8, 128]), [2, 3, 4])
+        log_pz3 = tf.reduce_sum(
+            tf.reshape(z3_out[1], [n_particles, -1, 4, 4, 256]), [2, 3, 4])
+
+        return log_px_z0 + log_pz0_z1 + log_pz1_z2 + log_pz2_z3 + log_pz3
 
     def reconst(self, latent, observed, given):
         """
@@ -98,18 +124,29 @@ class M1:
 
         :return: A Tensor. The joint log likelihoods.
         """
-        z0, z1 = latent['z0'], latent['z1']
+        z0, z1, z2, z3 = latent['z0'], latent['z1'], latent['z2'], latent['z3']
         x = observed['x']
         x = tf.tile(tf.expand_dims(x, 0), [n_particles, 1, 1])
-        z0_out, z1_out, x_out = self.model.get_output(self.zs + [self.x],
-                                                      inputs={self.zs[0]: z0,
-                                                              self.zs[1]: z1,
-                                                              self.x: x})
+        z0_out, z1_out, z2_out, z3_out, x_out = self.model.get_output(
+            self.zs + [self.x],
+            inputs={self.zs[0]: z0,
+                    self.zs[1]: z1,
+                    self.zs[2]: z2,
+                    self.zs[3]: z3
+                    })
         x_mean = x_out[0]
         log_px_z0 = tf.log(logistic.cdf(x + 1. / 256, x_mean, self.x_logsd) -
                            logistic.cdf(x, x_mean, self.x_logsd) + 1e-8)
         log_px_z0 = tf.reduce_sum(log_px_z0, -1)
-        return log_px_z0
+        log_pz0_z1 = tf.reduce_sum(
+            tf.reshape(z0_out[1], [n_particles, -1, 16, 16, 64]), [2, 3, 4])
+        log_pz1_z2 = tf.reduce_sum(
+            tf.reshape(z1_out[1], [n_particles, -1, 16, 16, 64]), [2, 3, 4])
+        log_pz2_z3 = tf.reduce_sum(
+            tf.reshape(z2_out[1], [n_particles, -1, 8, 8, 128]), [2, 3, 4])
+        log_pz3 = tf.reduce_sum(
+            tf.reshape(z3_out[1], [n_particles, -1, 4, 4, 256]), [2, 3, 4])
+        return [log_px_z0, log_pz0_z1, log_pz1_z2, log_pz2_z3, log_pz3]
 
 
 def q_net(x, n_xl, n_particles, is_training):
@@ -126,14 +163,29 @@ def q_net(x, n_xl, n_particles, is_training):
         x = tf.tile(tf.expand_dims(x, 0), [n_particles, 1, 1])
         x = tf.reshape(x, [-1, n_xl, n_xl, 3])
         lz0_x = layers.conv2d(x, 64, 3)
-        lz0_mean = layers.conv2d(lz0_x, 64, 5, stride=2, activation_fn=None)
-        lz0_logstd = layers.conv2d(lz0_x, 64, 5, stride=2, activation_fn=None)
+        lz0_x = layers.conv2d(lz0_x, 64, 3)
+        lz0_x = layers.conv2d(lz0_x, 64, 5, stride=2)
+        lz0_mean = layers.conv2d(lz0_x, 64, 3, activation_fn=None)
+        lz0_logstd = layers.conv2d(lz0_x, 64, 3, activation_fn=None)
         lz0 = Normal(lz0_mean, lz0_logstd)
         lz1_z0 = layers.conv2d(lz0.value, 64, 3)
+        lz1_z0 = layers.conv2d(lz1_z0, 64, 3)
         lz1_mean = layers.conv2d(lz1_z0, 64, 3, activation_fn=None)
         lz1_logstd = layers.conv2d(lz1_z0, 64, 3, activation_fn=None)
         lz1 = Normal(lz1_mean, lz1_logstd)
-        return variational, lz0, lz1
+        lz2_z1 = layers.conv2d(lz1.value, 64, 3)
+        lz2_z1 = layers.conv2d(lz2_z1, 64, 3)
+        lz2_z1 = layers.conv2d(lz2_z1, 128, 5, stride=2)
+        lz2_mean = layers.conv2d(lz2_z1, 128, 3, activation_fn=None)
+        lz2_logstd = layers.conv2d(lz2_z1, 128, 3, activation_fn=None)
+        lz2 = Normal(lz2_mean, lz2_logstd)
+        lz3_z2 = layers.conv2d(lz2.value, 128, 3)
+        lz3_z2 = layers.conv2d(lz3_z2, 128, 3)
+        lz3_z2 = layers.conv2d(lz3_z2, 256, 5, stride=2)
+        lz3_mean = layers.conv2d(lz3_z2, 256, 3, activation_fn=None)
+        lz3_logstd = layers.conv2d(lz3_z2, 256, 3, activation_fn=None)
+        lz3 = Normal(lz3_mean, lz3_logstd)
+        return variational, lz0, lz1, lz2, lz3
 
 
 if __name__ == "__main__":
@@ -149,7 +201,9 @@ if __name__ == "__main__":
     _, n_xl, _, n_channels = x_train.shape
     n_x = n_xl * n_xl * n_channels
     x_train = x_train.reshape((-1, n_x))
+    x_train -= np.mean(x_train, 0)
     x_test = x_test.reshape((-1, n_x))
+    x_test -= np.mean(x_test, 0)
     n_y = t_train.shape[1]
 
     # Define training/evaluation parameters
@@ -157,7 +211,7 @@ if __name__ == "__main__":
     ll_samples = 100
     epoches = 3000
     batch_size = 16
-    test_batch_size = 50
+    test_batch_size = 64
     iters = x_train.shape[0] // batch_size
     test_iters = x_test.shape[0] // test_batch_size
     test_freq = 10
@@ -171,21 +225,34 @@ if __name__ == "__main__":
     n_particles = tf.placeholder(tf.int32, shape=[], name='n_particles')
     x = tf.placeholder(tf.float32, shape=(None, n_x), name='x')
     n = tf.shape(x)[0]
-    optimizer = tf.train.AdamOptimizer(learning_rate_ph)
+    # optimizer = tf.train.AdamOptimizer(learning_rate_ph, epsilon=1e-8)
+    optimizer = AdamaxOptimizer(learning_rate_ph, beta1=0.9, beta2=0.999)
     model = M1(n_x, n, n_particles, is_training)
-    variational, lz0, lz1 = q_net(x, n_xl, n_particles, is_training)
-    z0_output, z1_output = variational.get_output([lz0, lz1])
-    z0_logpdf = tf.reduce_sum(tf.reshape(z0_output[1], [n_particles, n, 16, 16, 64]), [2, 3, 4])
-    z1_logpdf = tf.reduce_sum(tf.reshape(z1_output[1], [n_particles, n, 16, 16, 64]), [2, 3, 4])
+    variational, lz0, lz1, lz2, lz3 = q_net(x, n_xl, n_particles, is_training)
+    z0_output, z1_output, z2_output, z3_output = variational.get_output([lz0, lz1, lz2, lz3])
+    z0_logpdf = tf.reduce_sum(
+        tf.reshape(z0_output[1], [n_particles, n, 16, 16, 64]), [2, 3, 4])
+    z1_logpdf = tf.reduce_sum(
+        tf.reshape(z1_output[1], [n_particles, n, 16, 16, 64]), [2, 3, 4])
+    z2_logpdf = tf.reduce_sum(
+        tf.reshape(z2_output[1], [n_particles, n, 8, 8, 128]), [2, 3, 4])
+    z3_logpdf = tf.reduce_sum(
+        tf.reshape(z3_output[1], [n_particles, n, 4, 4, 256]), [2, 3, 4])
     lower_bound = tf.reduce_mean(advi(
         model, {'x': x}, {'z0': [z0_output[0], z0_logpdf],
-                          'z1': [z1_output[0], z1_logpdf]},
+                          'z1': [z1_output[0], z1_logpdf],
+                          'z2': [z2_output[0], z2_logpdf],
+                          'z3': [z3_output[0], z3_logpdf]},
         reduction_indices=0))
-    reconst = tf.reduce_mean(model.reconst({'z0': z0_output[0], 'z1': z1_output[0]}, {'x': x}, {}))
+    reconst = [tf.reduce_mean(i) for i in model.reconst(
+        {'z0': z0_output[0], 'z1': z1_output[0], 'z2': z2_output[0], 'z3': z3_output[0]},
+        {'x': x}, {})]
 
     log_likelihood = tf.reduce_mean(is_loglikelihood(
         model, {'x': x}, {'z0': [z0_output[0], z0_logpdf],
-                          'z1': [z1_output[0], z1_logpdf]},
+                          'z1': [z1_output[0], z1_logpdf],
+                          'z2': [z2_output[0], z2_logpdf],
+                          'z3': [z3_output[0], z3_logpdf]},
         reduction_indices=0))
     bits_per_dim = -lower_bound / n_x * 1. / np.log(2.)
     grads = optimizer.compute_gradients(bits_per_dim)
@@ -200,9 +267,9 @@ if __name__ == "__main__":
 
     infer = optimizer.apply_gradients(grads)
 
-    train_writer = tf.train.SummaryWriter('/tmp/zhusuan',
-                                          tf.get_default_graph())
-    train_writer.close()
+    # train_writer = tf.train.SummaryWriter('/tmp/zhusuan',
+    #                                       tf.get_default_graph())
+    # train_writer.close()
 
     total_size = 0
     params = tf.trainable_variables()
@@ -223,22 +290,35 @@ if __name__ == "__main__":
             bits = []
             update_ratios = []
             recs = []
+            recs1 = []
+            recs2 = []
+            recs3 = []
             for t in range(iters):
                 x_batch = x_train[t * batch_size:(t + 1) * batch_size]
-                _, lb, bit, update_ratio_, rec = sess.run([infer, lower_bound, bits_per_dim, update_ratio, reconst],
-                                      feed_dict={x: x_batch,
-                                                 learning_rate_ph: learning_rate,
-                                                 n_particles: lb_samples,
-                                                 is_training: True})
+                try:
+                    _, lb, bit, update_ratio_, rec0, rec1, rec2, rec3 = sess.run(
+                        [infer, lower_bound, bits_per_dim, update_ratio, reconst[0], reconst[1], reconst[2], reconst[3]],
+                        feed_dict={x: x_batch,
+                                   learning_rate_ph: learning_rate,
+                                   n_particles: lb_samples,
+                                   is_training: True})
+
+                except tf.errors.InvalidArgumentError as error:
+                    if ("NaN" in error.message) or ("Inf" in error.message):
+                        continue
+                    raise error
                 lbs.append(lb)
                 bits.append(bit)
                 update_ratios.append(update_ratio_)
-                recs.append(rec)
+                recs.append(rec0)
+                recs1.append(rec1)
+                recs2.append(rec2)
+                recs3.append(rec3)
                 # print('Iter {} ({:.1f}s): Lower bound = {} bits = {}'.format(
                 #     t, time.time()+time_epoch, lb, bit))
             time_epoch += time.time()
-            print('Epoch {} ({:.1f}s): Lower bound = {} bits = {} rec = {}'.format(
-                epoch, time_epoch, np.mean(lbs), np.mean(bits), np.mean(recs)))
+            print('Epoch {} ({:.1f}s): Lower bound = {} bits = {} rec = {} logpz0 = {} logpz1 = {} logpz2 = {}'.format(
+                epoch, time_epoch, np.mean(lbs), np.mean(bits), np.mean(recs), np.mean(recs1), np.mean(recs2), np.mean(recs3)))
             print('update ratio = {}'.format(np.mean(update_ratios)))
             if epoch % test_freq == 0:
                 time_test = -time.time()
