a868d4f41144b0f40a854f58eed8fcf533a82a09
xinmei9322 <luoyucen9322@qq.com>
2017-02-15 16:45:00 +0800

    iaf posterior

--- examples/ladder_vae.py
+++ examples/ladder_vae.py
@@ -23,6 +23,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 try:
     import zhusuan as zs
     from zhusuan.distributions import logistic
+    from zhusuan.utils import merge_dicts
 except:
     raise ImportError()
 
@@ -38,7 +39,7 @@ def ladder_vae(observed, n, n_particles, groups):
         h_top = tf.get_variable(name='h_top_p',
                                 shape=[1, 1, 1, groups[-1].num_filters],
                                 initializer=tf.constant_initializer(0.0))
-        h = downward(h_top, n, n_particles, groups)
+        h, _ = downward(h_top, n, n_particles, groups)
         h = tf.nn.elu(h)
         x = layers.conv2d_transpose(h, 3, kernel_size=5, stride=2,
                                     activation_fn=None, scope='x_mean')
@@ -54,19 +55,19 @@ def q_net(x, n_xl, n_particles, groups):
         x = tf.reshape(x, [-1, n_xl, n_xl, 3])
         n = tf.shape(x)[0]
         h = layers.conv2d(x, groups[0].num_filters, 5, 2, activation_fn=None)
-        h, qz_mean, qz_logstd = upward(h, n, n_particles, groups)
+        h, qz_mean, qz_logstd, qz_up_context = upward(h, n, n_particles, groups)
         h_top = tf.get_variable(name='h_top_q',
                                 shape=[1, 1, 1, groups[-1].num_filters],
                                 initializer=tf.constant_initializer(0.0))
-        h = downward(h_top, n, n_particles, groups,
-                     lateral_inputs=[qz_mean, qz_logstd])
-    z_names = list(six.iterkeys(qz_mean))
-    return variational, z_names
+        h, ar_z = downward(h_top, n, n_particles, groups,
+                           lateral_inputs=[qz_mean, qz_logstd, qz_up_context])
+    return variational, ar_z
 
 
 def upward(h, n, n_particles, groups):
     qz_mean = {}
     qz_logstd = {}
+    qz_up_context = {}
     for group_i, group in enumerate(groups):
         for block_i in range(group.num_blocks):
             name = 'group_%d/block_%d' % (group_i, block_i)
@@ -74,11 +75,11 @@ def upward(h, n, n_particles, groups):
             if group_i > 0 and block_i == 0:
                 stride = 2
             h1 = tf.nn.elu(h)
-            h1 = layers.conv2d(h1, group.num_filters + 2 * group.n_z, 3,
+            h1 = layers.conv2d(h1, 2 * group.num_filters + 2 * group.n_z, 3,
                                stride=stride, activation_fn=None,
                                scope=name + '_up_conv1')
-            qz_mean[name], qz_logstd[name], h1 = tf.split(
-                h1, [group.n_z] * 2 + [group.num_filters], axis=3)
+            qz_mean[name], qz_logstd[name], h1, qz_up_context[name] = tf.split(
+                h1, [group.n_z] * 2 + [group.num_filters] * 2, axis=3)
             h1 = tf.nn.elu(h1)
             h1 = layers.conv2d(h1, group.num_filters, kernel_size=3,
                                activation_fn=None, scope=name + '_up_conv2')
@@ -87,12 +88,13 @@ def upward(h, n, n_particles, groups):
                                   activation_fn=None,
                                   scope=name + '_resize_up')
             h += 0.1 * h1
-    return h, qz_mean, qz_logstd
+    return h, qz_mean, qz_logstd, qz_up_context
 
 
 def downward(h_top, n, n_particles, groups, lateral_inputs=None):
     h = tf.tile(h_top, [n * n_particles, groups[-1].map_size,
                         groups[-1].map_size, 1])
+    ar_z = {}
     for group_i, group in reversed(list(enumerate(groups))):
         for block_i in reversed(range(group.num_blocks)):
             name = 'group_%d/block_%d' % (group_i, block_i)
@@ -100,30 +102,52 @@ def downward(h_top, n, n_particles, groups, lateral_inputs=None):
             if group_i > 0 and block_i == 0:
                 stride = 2
             h1 = tf.nn.elu(h)
-            h1 = layers.conv2d_transpose(
-                h1, group.n_z * 2 + group.num_filters, 3,
-                activation_fn=None, scope=name + '_down_conv1')
-            h1, pz_mean, pz_logstd = tf.split(
-                h1, [group.num_filters] + [group.n_z] * 2, axis=3)
-
             if lateral_inputs:
-                qz_mean, qz_logstd = lateral_inputs
+                h1 = layers.conv2d_transpose(
+                    h1, group.num_filters * 2 + group.n_z * 2, 3,
+                    activation_fn=None, scope=name + '_down_conv1')
+                h1, pz_down_context, pz_mean, pz_logstd = tf.split(
+                    h1, [group.num_filters] * 2 + [group.n_z] * 2, axis=3)
+
                 pz_mean, pz_logstd = [
-                    tf.reshape(x, [n_particles, -1, group.map_size,
+                    tf.reshape(x, [n_particles, n, group.map_size,
                                    group.map_size, group.n_z])
                     for x in [pz_mean, pz_logstd]]
+                pz_down_context = tf.reshape(pz_down_context,
+                                             [n_particles, n, group.map_size,
+                                              group.map_size, group.num_filters])
+                qz_mean, qz_logstd, qz_up_context = lateral_inputs
                 post_z_mean = pz_mean + tf.expand_dims(qz_mean[name], 0)
                 post_z_logstd = pz_logstd + tf.expand_dims(qz_logstd[name], 0)
+                post_z_context = pz_down_context + \
+                                 tf.expand_dims(qz_up_context[name], 0)
                 post_z_mean, post_z_logstd = [
-                    tf.reshape(x, [-1, group.map_size, group.map_size,
-                                   group.n_z])
+                    tf.reshape(x, [n*n_particles, group.map_size,
+                                   group.map_size, group.n_z])
                     for x in [post_z_mean, post_z_logstd]]
+                post_z_context = tf.reshape(post_z_context,
+                                            [n*n_particles, group.map_size,
+                                             group.map_size, group.num_filters])
                 z_i = zs.Normal(name, post_z_mean, post_z_logstd)
+                z_i_samples = z_i.tensor
+                log_qz_i = z_i.log_prob(z_i_samples)
+                arw_mean, arw_logstd = ar_multiconv2d(
+                    z_i_samples, post_z_context, [group.num_filters,
+                                                  group.num_filters],
+                    [group.n_z, group.n_z], name=name + 'ar_multi_conv')
+                z_i_samples = (z_i_samples - arw_mean) / tf.exp(arw_logstd)
+                log_qz_i += arw_logstd
+                log_qz_i = tf.reduce_sum(
+                    tf.reshape(log_qz_i, [n_particles, n, -1]), -1)
+                ar_z[name] = [z_i_samples, log_qz_i]
             else:
+                h1 = layers.conv2d_transpose(
+                    h1, group.num_filters + group.n_z * 2, 3,
+                    activation_fn=None, scope=name + '_down_conv1')
+                h1, pz_mean, pz_logstd = tf.split(
+                    h1, [group.num_filters] + [group.n_z] * 2, axis=3)
                 z_i = zs.Normal(name, pz_mean, pz_logstd)
 
-            z_i = tf.reshape(z_i, [-1, group.map_size, group.map_size,
-                                   group.n_z])
             h1 = tf.concat([h1, z_i], 3)
             h1 = tf.nn.elu(h1)
             if stride == 2:
@@ -134,8 +158,63 @@ def downward(h_top, n, n_particles, groups, lateral_inputs=None):
                                          activation_fn=None,
                                          scope=name + '_down_conv2')
             h += 0.1 * h1
-    return h
+    return h, ar_z
 
+
+def get_linear_ar_mask(n_in, n_out, zerodiagonal=False):
+    assert n_in % n_out == 0 or n_out % n_in == 0, "%d - %d" % (n_in, n_out)
+
+    mask = np.ones([n_in, n_out], dtype=np.float32)
+    if n_out >= n_in:
+        k = int(n_out / n_in)
+        for i in range(n_in):
+            mask[i + 1:, i * k:(i + 1) * k] = 0
+            if zerodiagonal:
+                mask[i:i + 1, i * k:(i + 1) * k] = 0
+    else:
+        k = int(n_in / n_out)
+        for i in range(n_out):
+            mask[(i + 1) * k:, i:i + 1] = 0
+            if zerodiagonal:
+                mask[i * k:(i + 1) * k:, i:i + 1] = 0
+    return mask
+
+
+def get_conv_ar_mask(h, w, n_in, n_out, zerodiagonal=False):
+    l = int((h - 1) / 2)
+    m = int((w - 1) / 2)
+    mask = np.ones([h, w, n_in, n_out], dtype=np.float32)
+    mask[:l, :, :, :] = 0
+    mask[l, :m, :, :] = 0
+    mask[l, m, :, :] = get_linear_ar_mask(n_in, n_out, zerodiagonal)
+    return mask
+
+
+def ar_conv2d(name, x, num_filters, filter_size=(3, 3), stride=(1, 1),
+              pad="SAME", zerodiagonal=True):
+    h = filter_size[0]
+    w = filter_size[1]
+    n_in = int(x.get_shape()[-1])
+    n_out = num_filters
+    filter_shape = [h, w, n_in, n_out]
+    mask = tf.constant(get_conv_ar_mask(h, w, n_in, n_out, zerodiagonal))
+    with tf.variable_scope(name):
+        v = tf.get_variable("V", filter_shape, tf.float32)
+    w = v * mask
+    return tf.nn.conv2d(x, w, strides=[1, stride[0], stride[1], 1], padding=pad,
+                        use_cudnn_on_gpu=True)
+
+
+def ar_multiconv2d(input, context, n_h, n_out, name, nl=tf.nn.elu):
+    with tf.variable_scope(name):
+        for i, size in enumerate(n_h):
+            input = ar_conv2d("layer_%d" % i, input, size, zerodiagonal=False)
+            if i == 0:
+                input += context
+            input = nl(input)
+        return [
+            ar_conv2d("layer_out_%d" % i, input, size, zerodiagonal=True) * 0.1
+            for i, size in enumerate(n_out)]
 
 if __name__ == "__main__":
     tf.set_random_seed(1234)
@@ -156,7 +235,7 @@ if __name__ == "__main__":
 
     # Define training/evaluation parameters
     lb_samples = 1
-    epoches = 1000
+    epoches = 2000
     batch_size = 16 * FLAGS.num_gpus
     test_batch_size = 16 * FLAGS.num_gpus
     iters = x_train.shape[0] // batch_size
@@ -199,13 +278,7 @@ if __name__ == "__main__":
             log_px_z = tf.reduce_sum(log_px_z, -1)
             return log_px_z + log_pz
 
-        variational, z_names = q_net(x_part, n_xl, n_particles, groups)
-        qz_outputs = variational.query(z_names, outputs=True,
-                                       local_log_prob=True)
-        zs_ = [[qz_sample,
-                tf.reduce_sum(tf.reshape(log_qz, [n_particles, n, -1]), -1)]
-               for qz_sample, log_qz in qz_outputs]
-        latents = dict(zip(z_names, zs_))
+        variational, latents = q_net(x_part, n_xl, n_particles, groups)
         lower_bound = tf.reduce_mean(
             zs.advi(log_joint, {'x': x_obs}, latents, axis=0))
         bits_per_dim = -lower_bound / n_x * 1. / np.log(2.)
