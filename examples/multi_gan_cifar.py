069491007c575e29e32259abb6845c28a87aa37e
xinmei9322 <luoyucen9322@qq.com>
2017-03-22 17:24:36 +0800

    add check gradients

--- examples/multi_gan_cifar.py
+++ examples/multi_gan_cifar.py
@@ -59,9 +59,17 @@ def multi_gan(classifier, g_model, l_infer, u_infer):
     true_l = tf.ones([tf.shape(l_infer_class_logits)[0]], tf.int32)
     true_u = 2*tf.ones([tf.shape(u_infer_class_logits)[0]], tf.int32)
 
-    fake_g = tf.tile([[0., 0.5, 0.5]], [tf.shape(g_model_class_logits)[0], 1])
-    fake_l = tf.tile([[0.5, 0., 0.5]], [tf.shape(l_infer_class_logits)[0], 1])
-    fake_u = tf.tile([[0.5, 0.5, 0.]], [tf.shape(u_infer_class_logits)[0], 1])
+    # o_0' = - log (exp(o_1 -o_0) + exp(o_2 - o_0))
+    o_0, o_1, o_2 = tf.split(g_model_class_logits, 3, axis=1)
+    fake_g_logits = -tf.reduce_logsumexp(tf.stack([o_1 - o_0, o_2 - o_0],
+                                                  axis=1), axis=1)
+    o_0, o_1, o_2 = tf.split(l_infer_class_logits, 3, axis=1)
+    fake_l_logits = -tf.reduce_logsumexp(tf.stack([o_0 - o_1, o_2 - o_1],
+                                                  axis=1), axis=1)
+    o_0, o_1, o_2 = tf.split(u_infer_class_logits, 3, axis=1)
+    fake_u_logits = -tf.reduce_logsumexp(tf.stack([o_0 - o_2, o_1 - o_2],
+                                                  axis=1), axis=1)
+
     disc_losses_list = [
         tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
             labels=true_g,
@@ -71,29 +79,32 @@ def multi_gan(classifier, g_model, l_infer, u_infer):
             logits=l_infer_class_logits)),
         tf.reduce_mean(tf.reshape(
             tf.nn.sparse_softmax_cross_entropy_with_logits(
-                labels=true_u, logits=u_infer_class_logits),
+                labels=true_u,
+                logits=u_infer_class_logits),
             (-1, tf.shape(qy_u)[1])) * qy_u)
     ]
     disc_loss = sum(disc_losses_list)
 
-    gen_losses_list = [tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(
-            labels=fake_g,
-            logits=g_model_class_logits)),
-        tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            labels=fake_l,
-            logits=l_infer_class_logits)),
-        tf.reduce_mean(tf.reshape(tf.nn.softmax_cross_entropy_with_logits(
-            labels=fake_u,
-            logits=u_infer_class_logits), (-1, tf.shape(qy_u)[1])) * qy_u)
+    gen_losses_list = [
+        tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=tf.zeros_like(fake_g_logits),
+            logits=fake_g_logits)),
+        tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=tf.zeros_like(fake_l_logits),
+            logits=fake_l_logits)),
+        tf.reduce_mean(tf.reshape(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=tf.zeros_like(fake_u_logits),
+                logits=fake_u_logits),
+            (-1, tf.shape(qy_u)[1])) * qy_u)
     ]
     gen_loss = sum(gen_losses_list)
     return disc_loss, gen_loss, disc_losses_list, gen_losses_list
 
 
-@zs.reuse('model')
-def model(observed, n, n_x, n_y, n_z, is_training):
-    with zs.StochasticGraph(observed=observed) as model:
+@zs.reuse('px_yz')
+def px_yz(observed, n, n_x, n_y, n_z, is_training):
+    with zs.StochasticGraph(observed=observed) as px_yz:
         normalizer_params = {'is_training': is_training,
                              'updates_collections': None}
         z_mean = tf.zeros([n_z])
@@ -210,7 +221,7 @@ def qy_x(x, n_y, is_training):
     ly_x = layers.conv2d(ly_x, 256, 3, normalizer_fn=layers.batch_norm,
                          normalizer_params=normalizer_params)
     ly_x = layers.max_pool2d(ly_x, kernel_size=2)
-    ly_x = layers.dropout(ly_x, keep_prob=0.5)
+    ly_x = layers.dropout(ly_x, keep_prob=0.5, is_training=is_training)
     ly_x = layers.conv2d(ly_x, 512, 3, padding='VALID',
                          normalizer_fn=layers.batch_norm,
                          normalizer_params=normalizer_params)
@@ -243,7 +254,7 @@ def unlabeled(x, n_x, n_y, n_z, is_training):
         x = tf.reshape(tf.tile(tf.expand_dims(x, 1), [1, n_y, 1]), [-1, n_x])
         z_mean, z_logstd = qz_xy(x, y, n_z, is_training)
         z = zs.Normal('z', z_mean, z_logstd, reparameterized=True)
-    return x, y, z, qy
+    return x, y, z, qy, y_logits
 
 
 def unlabeled_test(x, n_y, n_z, is_training):
@@ -348,11 +359,10 @@ if __name__ == "__main__":
     test_iters = x_test.shape[0] // test_batch_size
     test_freq = iters
     save_freq = iters
-    print_freq = 100
+    print_freq = 200
     learning_rate = 0.0001
     anneal_lr_freq = 200
     anneal_lr_rate = 0.75
-    print_freq = 100
     recon_size = 50
     beta = 1.
 
@@ -370,91 +380,96 @@ if __name__ == "__main__":
     x_labeled_ph = tf.placeholder(tf.float32, shape=[None, n_x], name='x_l')
     y_labeled_ph = tf.placeholder(tf.float32, shape=[None, n_y], name='y_l')
     x_unlabeled_ph = tf.placeholder(tf.float32, shape=(None, n_x), name='x_u')
+    n = tf.shape(x_labeled_ph)[0]
 
-    def build_tower_graph(x_l, y_l, x_u, id_):
-        tower_x_l, tower_y_l, tower_x_u = [
-            i[id_ * tf.shape(i)[0] // FLAGS.num_gpus:
-                (id_ + 1) * tf.shape(i)[0] // FLAGS.num_gpus]
-            for i in [x_l, y_l, x_u]]
+    # Get the three groups of data
+    x_gen, y_prior, z_prior = px_yz(None, n, n_x, n_y, n_z, is_training)
+    z_l = labeled(x_labeled_ph, y_labeled_ph, n_z, is_training)
+    x_u, y_u, z_u, qy_u, qy_logits_u = unlabeled(x_unlabeled_ph, n_x, n_y, n_z,
+                                                 is_training)
 
-        n = tf.shape(tower_x_l)[0]
-        x_gen, y_prior, z_prior = model(None, n, n_x, n_y, n_z, is_training)
-        z_l = labeled(tower_x_l, tower_y_l, n_z, is_training)
-        x_u, y_u, z_u, qy_u = unlabeled(tower_x_u, n_x, n_y, n_z, is_training)
+    def classifier(tmp_x, tmp_y, tmp_z):
+        return discriminator(tmp_x, tmp_y, tmp_z, n_y, n_z, is_training)
 
-        def classifier(tmp_x, tmp_y, tmp_z):
-            return discriminator(tmp_x, tmp_y, tmp_z, n_y, n_z, is_training)
+    disc_loss, gen_loss, d_loss_list, g_loss_list = multi_gan(
+        classifier,
+        g_model={'x': x_gen, 'y': y_prior, 'z': z_prior},
+        l_infer={'x': x_labeled_ph, 'y': y_labeled_ph, 'z': z_l},
+        u_infer={'x': x_u, 'y': y_u, 'z': z_u, 'qy': qy_u})
 
-        disc_loss, gen_loss, d_loss_list, g_loss_list = multi_gan(
-            classifier,
-            g_model={'x': x_gen, 'y': y_prior, 'z': z_prior},
-            l_infer={'x': tower_x_l, 'y': tower_y_l, 'z': z_l},
-            u_infer={'x': x_u, 'y': y_u, 'z': z_u, 'qy': qy_u})
+    # q(y|x) through labeled data, classifier loss and accuracy
+    qy_logits_l = qy_x(x_labeled_ph, n_y, is_training)
+    qy_l = tf.nn.softmax(qy_logits_l)
+    pred_y = tf.argmax(qy_l, 1)
+    acc = tf.reduce_mean(
+        tf.cast(tf.equal(pred_y, tf.argmax(y_labeled_ph, 1)), tf.float32))
+    log_qy_x = zs.discrete.logpmf(y_labeled_ph, qy_logits_l)
+    log_qy_x = tf.reduce_mean(log_qy_x)
+    cls_loss = -beta * log_qy_x
 
-        # q(y|x) through labeled data, classifier loss and accuracy
-        qy_logits_l = qy_x(tower_x_l, n_y, is_training)
-        qy_l = tf.nn.softmax(qy_logits_l)
-        pred_y = tf.argmax(qy_l, 1)
-        acc = tf.reduce_mean(
-            tf.cast(tf.equal(pred_y, tf.argmax(tower_y_l, 1)), tf.float32))
-        log_qy_x = zs.discrete.logpmf(tower_y_l, qy_logits_l)
-        log_qy_x = tf.reduce_mean(log_qy_x)
-        cls_loss = -beta * log_qy_x
+    px_yz_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                       scope='px_yz')
+    qz_xy_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                       scope='qz_xy')
+    qy_x_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                      scope='qy_x')
+    disc_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                      scope='discriminator')
+    gen_var_list = px_yz_var_list + qz_xy_var_list + qy_x_var_list
 
-        gen_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                         scope='model') + \
-                       tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                         scope='qz_xy') + \
-                       tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                         scope='qy_x')
-        disc_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                          scope='discriminator')
+    disc_grads = optimizer.compute_gradients(disc_loss,
+                                             var_list=disc_var_list)
+    gen_grads = optimizer.compute_gradients(gen_loss,
+                                            var_list=gen_var_list)
+    cls_grads = optimizer.compute_gradients(cls_loss, var_list=qy_x_var_list)
 
-        disc_grads = optimizer.compute_gradients(disc_loss,
-                                                 var_list=disc_var_list)
-        gen_grads = optimizer.compute_gradients(gen_loss,
-                                                var_list=gen_var_list)
-        cls_grads = optimizer.compute_gradients(
-            cls_loss, var_list=tf.get_collection(
-                tf.GraphKeys.TRAINABLE_VARIABLES, scope='qy_x'))
-        grads = disc_grads + gen_grads + cls_grads
-        return grads, gen_loss, disc_loss, cls_loss, acc, \
-               d_loss_list, g_loss_list
-
-
-    tower_losses = []
-    tower_grads = []
-    tower_lists = []
-    for i in range(FLAGS.num_gpus):
-        with tf.device('/gpu:%d' % i):
-            with tf.name_scope('tower_%d' % i):
-                grads, gen_loss, disc_loss, cls_loss, acc, \
-                d_loss_list, g_loss_list = build_tower_graph(
-                    x_labeled_ph, y_labeled_ph, x_unlabeled_ph, i)
-                tower_losses.append([gen_loss, disc_loss, cls_loss, acc])
-                tower_grads.append(grads)
-                tower_lists.append(d_loss_list + g_loss_list)
-    gen_loss, disc_loss, cls_loss, acc = multi_gpu.average_losses(tower_losses)
-    each_loss = multi_gpu.average_losses(tower_lists)
-    grads = multi_gpu.average_gradients(tower_grads)
+    # Get losses and gradients
+    grads = disc_grads + gen_grads + cls_grads
     infer = optimizer.apply_gradients(grads)
+    each_loss = d_loss_list + g_loss_list
+
+    def check_grads(loss, var_list):
+        grads_to_be_check = optimizer.compute_gradients(loss, var_list=var_list)
+        grads_to_be_check_ = [g for g, v in grads_to_be_check if g is not None]
+        grads_to_be_check_mean = tf.reduce_mean(
+            tf.stack([tf.reduce_mean(tf.square(g)) for g in grads_to_be_check_]))
+        grads_to_be_check_var = tf.reduce_mean(
+            tf.stack([tf.nn.moments(g, list(range(0, g.get_shape().ndims)))[1]
+                      for g in grads_to_be_check_]))
+        return grads_to_be_check_mean, grads_to_be_check_var
+
+
+    check_qyx_grads = check_grads(gen_loss, var_list=qy_x_var_list)
+    check_qyx_grads_u = check_grads(g_loss_list[2], var_list=qy_x_var_list)
+    check_cls_grads = check_grads(cls_loss, var_list=qy_x_var_list)
+    check_px_grads = check_grads(gen_loss, var_list=px_yz_var_list)
+    check_qzxy_grads_l = check_grads(g_loss_list[1], var_list=qz_xy_var_list)
+    check_qzxy_grads_u = check_grads(g_loss_list[2], var_list=qz_xy_var_list)
+    check_qzxy_grads_g = check_grads(gen_loss, var_list=qz_xy_var_list)
+
+    check_list = []
+    for i in [check_qyx_grads, check_qyx_grads_u, check_cls_grads,
+              check_px_grads, check_qzxy_grads_l, check_qzxy_grads_u,
+              check_qzxy_grads_g]:
+        check_list += i
+
     # eval generation
-    eval_x_gen, _, _ = model({'y': y_labeled_ph},
-                             2* recon_size, n_x, n_y, n_z, is_training)
+    eval_x_gen, _, _ = px_yz({'y': y_labeled_ph},
+                             2 * recon_size, n_x, n_y, n_z, is_training)
     # eval reconstruction, unlabeled data
     eval_y, eval_y_gen_onehot, eval_z_gen = unlabeled_test(
         x_unlabeled_ph, n_y, n_z, is_training)
-    eval_x_recon, _, _ = model({'y': eval_y_gen_onehot, 'z': eval_z_gen},
-                                tf.shape(x_unlabeled_ph)[0], n_x, n_y, n_z,
-                                is_training)
+    eval_x_recon, _, _ = px_yz({'y': eval_y_gen_onehot, 'z': eval_z_gen},
+                               tf.shape(x_unlabeled_ph)[0], n_x, n_y, n_z,
+                               is_training)
+
     params = tf.trainable_variables()
     for i in params:
         print(i.name, i.get_shape())
 
     # Run the inference
-    with multi_gpu.create_session() as sess:
+    with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        # saver.restore(sess, 'results/ali/ali.epoch.10.iter.1562.ckpt')
         for epoch in range(1, epoches + 1):
             if epoch % anneal_lr_freq == 0:
                 learning_rate *= anneal_lr_rate
@@ -462,8 +477,7 @@ if __name__ == "__main__":
             indices = np.random.permutation(x_unlabeled.shape[0])
             x_unlabeled = x_unlabeled[indices]
             t_unlabeled = t_unlabeled[indices]
-            gen_losses, disc_losses, cls_losses, accs, each_losses = \
-                [], [], [], [], []
+            fetches = []
             time_train = -time.time()
             for t in range(iters):
                 iter = t + 1
@@ -476,27 +490,33 @@ if __name__ == "__main__":
                 y_unlabeled_batch = t_unlabeled[t * batch_size:
                 (t + 1) * batch_size]
 
-                _, g_loss, d_loss, c_loss, c_acc = sess.run(
-                    [infer, gen_loss, disc_loss, cls_loss, acc],
-                    feed_dict={x_labeled_ph: x_labeled_batch,
-                               y_labeled_ph: y_labeled_batch,
-                               x_unlabeled_ph: x_unlabeled_batch,
-                               learning_rate_ph: learning_rate,
-                               is_training: True})
-                gen_losses.append(g_loss)
-                disc_losses.append(d_loss)
-                cls_losses.append(c_loss)
-                accs.append(c_acc)
+                ft = sess.run([infer, gen_loss, disc_loss, cls_loss, acc]
+                              + check_list + each_loss,
+                              feed_dict={x_labeled_ph: x_labeled_batch,
+                                         y_labeled_ph: y_labeled_batch,
+                                         x_unlabeled_ph: x_unlabeled_batch,
+                                         learning_rate_ph: learning_rate,
+                                         is_training: True})
+                fetches.append(ft[1:])
 
                 if iter % print_freq == 0:
+                    pt_fetches = np.mean(fetches, axis=0)
                     print('Epoch={} Iter={} ({:.3f}s/iter): '
                           'Gen loss = {:.3f} Disc loss = {:.3f} '
-                          'Cls loss = {:.3f} Acc = {:.2f}'.
+                          'Cls loss = {:.3f} Acc = {:.2f}\n'
+                          'Check qy_x Grads, Mean = {}, Var = {}\n'
+                          'Check qy_x_u Grads, Mean = {}, Var = {}\n'
+                          'Check cls Grads, Mean = {}, Var = {}\n'
+                          'Check p_x Grads, Mean = {}, Var = {}\n'
+                          'Check qzxy_grads_l Grads, Mean = {}, Var = {}\n'
+                          'Check qzxy_grads_u Grads, Mean = {}, Var = {}\n'
+                          'Check qzxy_grads_g Grads, Mean = {}, Var = {}\n'
+                          'd_g = {:.3f} d_l = {:.3f} d_u = {:.3f}\n'
+                          'g_g = {:.3f} g_l = {:.3f} g_u = {:.3f}'.
                           format(epoch, iter,
                                  (time.time() + time_train) / print_freq,
-                                 np.mean(gen_losses), np.mean(disc_losses),
-                                 np.mean(cls_losses), np.mean(accs)*100.))
-                    gen_losses, disc_losses, cls_losses, accs = [], [], [], []
+                                 *pt_fetches))
+                    fetches = []
 
                 if iter % test_freq == 0:
                     time_test = -time.time()
@@ -504,7 +524,8 @@ if __name__ == "__main__":
                                           feed_dict={
                                               y_labeled_ph:y_gen,
                                               is_training: False})
-                    gen_images = np.reshape(gen_images, [-1, n_xl, n_xl, n_channels])
+                    gen_images = np.reshape(gen_images, [-1, n_xl, n_xl,
+                                                         n_channels])
                     name = "gen/epoch.{}.iter.{}.png".format(epoch, iter)
                     name = os.path.join(result_path, name)
                     utils.save_image_collections(gen_images, name,
@@ -533,7 +554,8 @@ if __name__ == "__main__":
                         epoch, iter)
                     name = os.path.join(result_path, name)
                     x_batch = np.reshape(x_batch, [-1, n_xl, n_xl, n_channels])
-                    recon_images = np.reshape(recon_images, [-1, n_xl, n_xl, n_channels])
+                    recon_images = np.reshape(recon_images, [-1, n_xl, n_xl,
+                                                             n_channels])
                     utils.save_contrast_image_collections(x_batch,
                                                           recon_images, name,
                                                           scale_each=True)
@@ -545,9 +567,3 @@ if __name__ == "__main__":
 
                 if iter % print_freq == 0:
                     time_train = -time.time()
-
-
-
-
-
-
